6a7e621fbff1224ccb5528ecc967853b38c99e5e
mahaveer maharana <38815657+mahaveermaharana@users.noreply.github.com>
2018-04-28 22:29:44 +0530

    Add files via upload

---
+++ lab.java
@@ -0,0 +1,22 @@
+class lab 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World!");
+		Hello h = new hello();
+		
+	}
+}
+
+class Hello
+{
+	int a = 10;
+	static{
+		System.out.println("a");
+		
+}
+void show(){
+	System.out.println("hello");
+}
+}
+}
